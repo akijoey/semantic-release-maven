6a92f5e3194fd00f383878e1cc3de48b9adaaa51
akijoey <akijoey@akijoey.com>
2021-09-08 19:25:45 +0800

    fix: debug prepare

--- .releaserc.js
+++
@@ -1,8 +0,0 @@
-// .releaserc.js
-
-const config = '@akijoey/semantic-release-config'
-
-module.exports = {
-  extends: require.resolve(config),
-  plugins: [...require(config).plugins, '@semantic-release/npm']
-}
